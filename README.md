2ebcd36296baae04693bac22d68184a94d1fc0a6
SaiNadh001 <saibaba.nadh@dreamorbit.com>
2015-09-14 18:58:29 +0530

    Added sample TestCases

---
+++ README.md
@@ -0,0 +1,19 @@
+# Frisby-API-Testing
+Rest API Testing
+
+##Synopsis
+
+Frisby API Testing
+
+##Installation
+
+* npm install
+* (or)
+* npn install -g frisby
+* npm install -g jasmine-node
+
+##API Reference
+
+##License
+
+The MIT License (MIT)
---
+++ TestCase2_spec.js
@@ -0,0 +1,37 @@
+var frisby=require('frisby');
+
+
+
+frisby.create('Post JSON string as body')
+  .post('http://'+your_host+'/api/v1/user_authentication/login?api_token='+api_token, {
+  	"email": email,
+ 		"password": password
+ 	}, {json: true})
+ 	.inspectBody()
+  .expectHeaderContains('Content-Type', 'json')
+  .expectJSONTypes('', {
+    user: {
+      id: Number,
+      name: String,
+      email: String,
+      auth_token: String,
+      driver_number: String 
+    }
+  })
+  .afterJSON(function(json) {
+
+    frisby.create('Get Routes') 
+      .get('http://'+your_host+'/api/v1/user_shift_detail_logs/get_shift_detail?api_token='+api_token+'&auth_token='+json.user.auth_token)
+      // .inspectBody()
+      .afterJSON(function(json) {
+        console.log(json)
+      })
+      .toss()
+
+    frisby.create('logout the session') 
+      .get('http://'+your_host+'/api/v1/user_authentication/logout?api_token='+api_token+'&auth_token='+json.user.auth_token)
+      .inspectBody()
+      .toss()
+    
+	})
+.toss()
