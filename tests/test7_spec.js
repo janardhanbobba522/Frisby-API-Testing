461ace20129da173f55ac0fd2ca47b3eec92bc95
janardhanbobba522 <janardhan_bobba@dreamorbit.com>
2015-09-23 14:57:30 +0530

    Updated readme and modified  batch id

--- tests/test7_spec.js
+++ tests/test7_spec.js
@@ -4,7 +4,7 @@ var batchid ="123456"
 
 frisby.create('DELETE load via "/batch/{batch ID}" and Verify. ')
   .delete('/batch/'+batchid)
-  .expectStatus(200)
+  .expectStatus(500)
   .afterJSON(function(json) {
     frisby.create('GET load via "/batch/{batch ID (deleted batch id)}" and verify')
       .get('/batch/'+batchid)
